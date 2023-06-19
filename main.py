e271a6dcf436139f5f650d064147b5b439d583ac
unknown <petter@DESKTOP-CRO15S5.domain12.local>
2023-06-19 09:33:12 +0200

    Started adding random googling

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 import time 
 import mouse
-
 import random 
+import pyautogui
+import pandas as pd
+
+googleList = pd.read_csv("C:/Users/petter/Desktop/coding/python/imWorkingIPromise/seartch.csv")
 
 def sleepRandint():
     time.sleep(random.uniform(0, 1.5))
-
 def moveWheel(range_, minSleep): 
     for i in range(int(range_)):
         mouse.wheel(1)
@@ -30,48 +32,77 @@ def moveWheel(range_, minSleep):
         time.sleep(minSleep)
 
     sleepRandint()
-
-
-def moveMouse():
-    mosePosZ = mouse.get_position()[1] 
-    if mosePosZ > 800:
-        mouse.move(random.randint(-100, 100), random.randint(-400, 0), absolute=False, duration=random.uniform(0.1, 0.3))
+def moveMouse(pos=None, speed=None):
+        
+    if pos == None and speed == None:
+        mosePosZ = mouse.get_position()[1] 
+        if mosePosZ > 800:
+            mouse.move(random.randint(-100, 100), random.randint(-400, 0), absolute=False, duration=random.uniform(0.1, 0.3))
+        else: 
+            mouse.move(random.randint(-100, 100), random.randint(-500, 300), absolute=False, duration=random.uniform(0.1, 0.5))
     else: 
-        mouse.move(random.randint(-100, 100), random.randint(-500, 300), absolute=False, duration=random.uniform(0.1, 0.5))
+        print(mouse.get_position())
+        mouse.move(pos[0], pos[1], absolute=False, duration=speed)
+        print(mouse.get_position())
 
+
+
+
+def google(googleChanse): 
+
+    if random.randint(0, 100) < googleChanse:
+        random_item = googleList.sample(n=1)
+        search_string = random_item["seartch"].values[0]
+
+
+
+        pyautogui.hotkey('ctrl', 't') # PRESSES CTR + T (NEW PAGE)
+        pyautogui.click(-1100, 52) # PRESSES THE GOOGLE CHROME SEARTCH BAR
+        
+        for char in list(search_string): # LOOPS OVER ALL OF THE CHARACHTERS OF THE STRING
+            pyautogui.press(char) # ENTERS THE LETTERS
+            time.sleep(random.uniform(0.05, 0.3)) # SLEEPS A BIT
+
+        pyautogui.press("enter")
+
+        return 1
+    return 0    
+
+
+time.sleep(2)
 i=0
 while True:
 
 
-    i+=1
-    if random.randint(0, 100) < 30:
-        moveWheel(random.randint(0, 30), random.uniform(0, 0.1))
-    else: 
-        moveMouse()
+    #i+=1
+    #if random.randint(0, 100) < 30:
+    #    moveWheel(random.randint(0, 30), random.uniform(0, 0.1))
+    #else: 
+    #    moveMouse()
+#
+    #
+    #if random.randint(0, 100) < 90 and mouse.get_position()[1] <= 90:
+    #    if random.randint(0, 100) < 50:
+    #        mouse.move(40, 0, absolute=False, duration=random.uniform(0.25, 0.5))
+    #    else: 
+    #        mouse.move(0, 40, absolute=False, duration=random.uniform(0.25, 0.5))
+#
+#
+    #    mouse.click("left")
+    #    time.sleep(0.5)
+    #    mouse.move(0, 800, absolute=False, duration=random.uniform(0.25, 0.5))
+    #
+    #elif random.randint(0, 100) < 85:
+    #    mouse.click("left")
 
-    
-    if random.randint(0, 100) < 90 and mouse.get_position()[1] <= 90:
-        if random.randint(0, 100) < 50:
-            mouse.move(40, 0, absolute=False, duration=random.uniform(0.25, 0.5))
-        else: 
-            mouse.move(0, 40, absolute=False, duration=random.uniform(0.25, 0.5))
 
-
-        mouse.click("left")
-        time.sleep(0.2)
-        mouse.move(0, 800, absolute=False, duration=random.uniform(0.25, 0.5))
-    
-    elif random.randint(0, 100) > 85:
-        mouse.click("left")
-
-
-
+    google(100)
 
     print(i)
     print(mouse.get_position())
-    if i >= 10: 
 
-        pass
+    
+
 
 
 
